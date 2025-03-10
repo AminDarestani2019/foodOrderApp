5b3b67feb0ae581844d572f3c6470a024c1ad2ff
Amin Darestani <darestani.amin@gmail.com>
2025-03-10 16:51:15 +0330

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
+import CartContext from '../store/CartContext.jsx';
+import { useContext } from 'react';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+
+export default function Header(){
+    const userProgressCtx = useContext(UserProgressContext);
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems,item)=>{
+        return totalNumberOfItems + item.quantity;
+    },0);
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
+
+    return (
+    <header id="main-header">
+        <div id="title">
+            <img src={logoImg} alt='A restaurant' />
+            <h1>REACTFood</h1>
+        </div>
+        <nav>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
+        </nav>
+    </header>);
+}
